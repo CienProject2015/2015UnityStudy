98552181b57e66be59e4d1232669bffb6cde7b56
NakOh <kk0704@naver.com>
2015-04-06 23:39:49 +0900

    효빈이형이 준 Assets 추가

---
+++ Assets/02. Scripts/TestPlayerCtrl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestPlayerCtrl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += transform.forward * Input.GetAxis ("Vertical");
+	}
+}
---
+++ Assets/02. Scripts/testAnim.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testAnim : MonoBehaviour {
+	
+	private float h, v;
+	
+	public Animation _animation;
+	
+	private bool shoot = false;
+	private bool bulletShoot = false;
+	private float shootTimer = 0;
+	
+	void Update () {
+		if(Input.GetKey("w")||Input.GetKey("s")){
+			_animation.CrossFade("walk", 0.3f);
+		}else{
+			if(shoot){
+				if(!_animation.isPlaying){
+					shoot = false;
+				}
+			}else{
+				_animation.CrossFade("stand", 0.3f);
+				//_animation.clip = anim.stand;
+				//_animation.Play();
+			}
+		}
+
+	}
+	
+	
+	
+}
+
+[System.Serializable]
+public class Anim{
+	public AnimationClip walk, stand, shoot;
+}
