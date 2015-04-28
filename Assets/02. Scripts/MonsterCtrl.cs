aa3c63f3dd9d5578bb50c543e8089adb42ca088c
NakOh <kk0704@naver.com>
2015-04-29 01:49:20 +0900

    Chapter6 Monster 제작 및 Nav 구현

---
+++ Assets/02. Scripts/MonsterCtrl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterCtrl : MonoBehaviour {
+	private Transform monsterTr;
+	private Transform playerTr;
+	private NavMeshAgent nvAgent;
+
+	// Use this for initialization
+	void Start () {
+		monsterTr = this.gameObject.GetComponent<Transform> ();
+		playerTr = GameObject.FindWithTag ("Player").GetComponent<Transform> ();
+		nvAgent = this.gameObject.GetComponent<NavMeshAgent> ();
+
+		nvAgent.destination = playerTr.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
